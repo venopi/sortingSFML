f01d4be52f5314decd89c07c7138448823db6847
venopi <100430682+venopi@users.noreply.github.com>
2022-03-11 16:33:53 +0100

    Add files via upload
    Veränderungen:
    -der Übersicht zugute wurde eine Klasse "draw_bars" erstellt, die aus beliebig großen Arrays Säulen erstellt
        ->demnach Funktion: "draw_new_bars" entfernt und "draw_rand_bars" wurde zu "draw_bars" 
    
    -Funktion: "printarray" hinzugefügt - diente als HIlfe bei der Fehlersuche
    
    -Funktion: "sort" wurde umgeschrieben, sie sortiert das gegebene Array und gibt es dann zurück, Die Funktion muss öfters aufgerufen werden, da mehrere durchläufe gebraucht werden um das Array vollständig zu sortieren.
    
    -Funktion "check" hinzugefügt - sie überprüft nach einem Sortierungsvorgang ob das Array vollständig sortiert ist. Wenn nicht, wird die "sort" Funktion wieder aufgerufen um das Array weiterhin zu sortieren. Wenn der Sortierungsvorgang abgeschlossen ist wird 1 zurückgegeben.
    
    -das Programm wartet nach der Sortierung auf eine Nutzereingabe. Wird dann "s" gedrückt werden die sortierten Säulen visualisiert

--- SFML/SFML.cpp
+++ SFML/SFML.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <random>
-
+#include <ctime>
 
 int h = 512;
 int w = 512;
@@ -10,46 +10,48 @@ sf::RenderWindow window(sf::VideoMode(h, w), "SFML Test", sf::Style::Close | sf:
 
 int randomnumber()
 {
+
 	int randnum = rand() % 500 + 10;
 
 	return randnum;
 }
 
-
-void draw_bars()
+void printarray(int * arr, int size)
 {
+	for (int i = 0; i < size; i++)
+	{
+		std::cout<< "#"<< i <<"#________" << arr[i] << std::endl;
+	}
+}
 
-	//create bars with random height
 
-
-
+int draw_bars(int * arr, int size)
+{
+	printarray(arr, size);
+	//draw bars with random height. Height will be taken from an array of random numbers
+	
 	double barposh, barposw;
 	int barh, barw;
 
-	barw = 20;
-	//barh = rand() % 500 + 1;
+	barw = 10;
 
-	//barposh = h - barh;				//makes sure bars are drawn an bottom edge of window
 	barposw = 5;
+	
+	int barnum = size;
 
-	int barnum = 10;
-
+	int gap_between_bars = 5;
 
 	window.clear(sf::Color::Black);
 
-	//sf::RectangleShape bar(sf::Vector2f(barw, barh));
-
 	for (int i = 0; i < barnum; i++)
 	{
-		barh = randomnumber();
-
-		sf::RectangleShape bars_array[10];
+		barh = arr[i];
 
 		sf::RectangleShape bar(sf::Vector2f(barw, barh));
 
-		bars_array[i] = bar;
+		arr[i] = barh;
 
-		barposh = h - barh;													//makes sure bars are drawn an bottom edge of window
+		barposh = h - barh;									//makes sure bars are drawn an bottom edge of window
 
 		bar.setPosition(barposw, barposh);
 
@@ -57,24 +59,95 @@ void draw_bars()
 
 		window.draw(bar);
 
-		barposw = barposw + barw + 30;
+		barposw = barposw + barw + gap_between_bars;
+
 
 	}
 
 	
 	window.display();
 
+	return 0;										//an array with the heights of the bars gets returned
+}
+
+
+
+int * sort(int * array, int size)
+{	
+	int *copiedarray;
+	copiedarray = array;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (i == (size-1))
+		{
+			std::cout << "sorted" << std::endl;
+			return array;
+			
+		}
+		else if (array[i] > array[i+1])
+		{
+
+			int temp;
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+
+		}
+		
+	}
+
+	
 }
 
 
 
 
+int check(int * array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout <<"_______________________" << array[i] << std::endl;
+
+		
+
+			if (array[i] > array[i + 1] && i != (size - 1)) 
+				return 0;									//sort fail
+
+			else if (i == (size - 1))
+				return 1;									//sort success
+
+	}
+
+}
+
 int main()
 {
+	srand(time(0));
 
-	draw_bars();
+	int* sorted_arr;
 
+	int arr[30];
+	
+	for (int i = 0; i < 30; i++)
+	{
+		arr[i] = randomnumber();
+	}
 
+	draw_bars(arr, 30);
+
+	sorted_arr = sort(arr, 30);
+
+	printarray(sorted_arr, 30); 
+
+	check(sorted_arr, 30);
+
+	while (check(sorted_arr, 30) == 0)
+	{
+		sort(sorted_arr, 30);
+	}
+
+	std::cout << "Success" << std::endl;
 
 	while (window.isOpen())
 	{
@@ -97,15 +170,11 @@ int main()
 			case sf::Event::KeyPressed:
 				if (evnt.key.code == sf::Keyboard::S)
 				{
-					//sort();
+					draw_bars(sorted_arr, 30);
 				}
 				
 			}
-			/*
-			if (evnt.type == evnt.Closed) {
 
-				window.close();
-			} */
 
 
 
@@ -115,6 +184,8 @@ int main()
 
 
 	}
+
+
 
     return 0;
 }
